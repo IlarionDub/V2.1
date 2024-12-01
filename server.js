4f703fb4f69cb51a84e2160cbe3d6e6a5abce33c
IlarionDub <ilariondub5@gmail.com>
2024-12-01 16:16:42 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,20 @@
+const cors = require('cors');
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+server.use(cors());
+server.use(middlewares);
+server.use(router);
+
+server.listen(3000, () => {
+    console.log('JSON Server is running on port 3000');
+});
+
+
+
+
+server.listen(3000, () => {
+    console.log('JSON Server is running on port 3000');
+});
